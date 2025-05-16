d379ab23e8fb83eab1749a8dacbe098225bf93c0
Leonardo Benvenutti <140168119+8462leo@users.noreply.github.com>
2025-05-16 09:52:38 -0300

    Update About.jsx
    Ajustando a foto

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -87,7 +87,7 @@ const About = () => {
               <img
                 className="w-full h-auto"
                 alt="Foto profissional trabalhando"
-                src="src/assets/Gemini_Generated_Image_c9wa1nc9wa1nc9wa.png"
+                src="https://github.com/8462leo/Portifolio_Final/blob/main/src/assets/Gemini_Generated_Image_c9wa1nc9wa1nc9wa.png?raw=true"
               />
             </div>
             <div className="absolute -bottom-4 -right-4 w-full h-full border-4 border-primary/10 rounded-lg -z-10"></div>
